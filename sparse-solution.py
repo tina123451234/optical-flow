6ccc76b36172d063afe88beee97ada88d65b403c
tina123451234 <84490469+tina123451234@users.noreply.github.com>
2022-06-29 14:51:34 -0400

    Update sparse-solution.py

--- sparse-solution.py
+++ sparse-solution.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 # Parameters for Shi-Tomasi corner detection
-feature_params = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
+feature_params = dict(maxCorners = 0, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # The video feed is read in as a VideoCapture object

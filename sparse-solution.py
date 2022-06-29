f38e0af782dc6ed19c6e7ef38776be6de074750f
tina123451234 <84490469+tina123451234@users.noreply.github.com>
2022-06-29 12:56:21 -0400

    Update sparse-solution.py

--- sparse-solution.py
+++ sparse-solution.py
@@ -2,13 +2,13 @@ import cv2 as cv
 import numpy as np
 
 # Parameters for Shi-Tomasi corner detection
-feature_params = dict(maxCorners = 300, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
+feature_params = dict(maxCorners = 500, qualityLevel = 0.2, minDistance = 2, blockSize = 7)
 # Parameters for Lucas-Kanade optical flow
 lk_params = dict(winSize = (15,15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 # The video feed is read in as a VideoCapture object
 cap = cv.VideoCapture("shibuya.mp4")
 # Variable for color to draw optical flow track
-color = (0, 255, 0)
+color = (0, 500, 0)
 # ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
 ret, first_frame = cap.read()
 # Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
